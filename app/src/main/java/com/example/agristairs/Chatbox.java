b1e6a1d6c53442f60b9c5a639f529bec455ba234
JaHid4046 <jh9988027@gmail.com>
2023-03-09 00:11:21 +0600

    uiux 0.6

--- app/src/main/java/com/example/agristairs/Chatbox.java
+++ app/src/main/java/com/example/agristairs/Chatbox.java
@@ -3,16 +3,12 @@ package com.example.agristairs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ProgressBar;
 
-import java.util.Timer;
-
-public class Splash_Screen extends AppCompatActivity {
-
+public class Chatbox extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_chatbox);
     }
 }
