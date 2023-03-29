e7f76542d3d172679daeaa70e614aa1a4aed79b3
ARisHERE <mdahad.mma2017@gmail.com>
2023-03-29 21:29:30 +0600

    uiux correction

--- app/src/main/java/com/example/agristairs/MainActivity.java
+++ app/src/main/java/com/example/agristairs/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.agristairs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    // Declare variables
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    private static final int SHAKE_THRESHOLD = 600;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        mAccelLast = mAccelCurrent;
+        mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+        float delta = mAccelCurrent - mAccelLast;
+        mAccel = mAccel * 0.9f + delta; // perform low-cut filter
+        if (mAccel > SHAKE_THRESHOLD) {
+            // Open the app
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.example.agristairs");
+            if (intent != null) {
+                startActivity(intent);
+            } else {
+                // App not found
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
 }
